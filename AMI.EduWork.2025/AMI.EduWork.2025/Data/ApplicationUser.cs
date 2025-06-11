40efbc6ecaccdba9760ae9d6c4b604fad2d96b9d
irosic21 <rosicivo12@gmail.com>
2025-06-11 17:18:50 +0200

    Initial commit

---
+++ AMI.EduWork.2025/AMI.EduWork.2025/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace AMI.EduWork._2025.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
