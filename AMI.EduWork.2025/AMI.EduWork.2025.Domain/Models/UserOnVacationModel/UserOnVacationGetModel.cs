d729820e7d1d54a0c629ccaf06d2743d558b2bae
AdrianBednarek1 <add.bednarek@gmail.com>
2025-06-20 12:13:31 +0200

    Created models for user on vacation

---
+++ AMI.EduWork.2025/AMI.EduWork.2025.Domain/Models/UserOnVacationModel/UserOnVacationGetModel.cs
@@ -0,0 +1,21 @@
+﻿using AMI.EduWork._2025.Data;
+using AMI.EduWork._2025.Data.Entities;
+using AMI.EduWork._2025.Domain.Models.VacationModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMI.EduWork._2025.Domain.Models.UserOnVacationModel
+{
+    public class UserOnVacationGetModel
+    {
+        //public string UserGetModel { get; set; }
+        public VacationGetModel _VacationGetModel { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ AMI.EduWork.2025/AMI.EduWork.2025.Domain/Models/UserOnVacationModel/UserOnVacationUpdateModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AMI.EduWork._2025.Domain.Models.UserOnVacationModel
+{
+    public class UserOnVacationUpdateModel
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
