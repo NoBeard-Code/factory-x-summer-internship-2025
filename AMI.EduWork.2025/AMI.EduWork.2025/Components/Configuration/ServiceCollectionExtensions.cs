f78ec9b3911f341bd16a349d1c4437f5738641c0
AdrianBednarek1 <add.bednarek@gmail.com>
2025-06-21 13:08:53 +0200

    Created ConfigureServices extension

---
+++ AMI.EduWork.2025/AMI.EduWork.2025/Components/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,64 @@
+﻿using AMI.EduWork._2025.Components.Account;
+using AMI.EduWork._2025.Data;
+using AMI.EduWork._2025.Data.Migrations;
+using AMI.EduWork._2025.Data.Repository;
+using AMI.EduWork._2025.Domain;
+using AMI.EduWork._2025.Domain.Interfaces.Repository;
+using AMI.EduWork._2025.Domain.Interfaces.Service;
+using AMI.EduWork._2025.Domain.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace AMI.EduWork.Configuration;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddRazorComponents()
+                .AddInteractiveServerComponents()
+                .AddInteractiveWebAssemblyComponents()
+                .AddAuthenticationStateSerialization();
+
+        services.AddCascadingAuthenticationState();
+        services.AddScoped<IdentityUserAccessor>();
+        services.AddScoped<IdentityRedirectManager>();
+        services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultScheme = IdentityConstants.ApplicationScheme;
+            options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+        })
+            .AddIdentityCookies();
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+        services.AddDatabaseDeveloperPageExceptionFilter();
+
+        services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddSignInManager()
+            .AddDefaultTokenProviders();
+
+        services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+        services.AddTransient<DataSeeder>();
+
+        //Repositories
+        services.AddScoped<IWorkDayRepository, WorkDayRepository>();
+        services.AddScoped<IVacationRepository, VacationRepository>();
+        services.AddScoped<IUserOnVacationRepository, UserOnVacationRepository>();
+        services.AddScoped<IContractRepository, ContractRepository>();
+
+        //Services
+        services.AddScoped<IWorkDayService, WorkDayService>();
+        services.AddScoped<IVacationService, VacationService>();
+        services.AddScoped<IContractService, ContractService>();
+
+        return services;
+    }
+}
+
