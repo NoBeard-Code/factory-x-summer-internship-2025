5b20f84c1c900778a9ae087770f1b77ae9a803ea
mstura21-FOI <mustra769@gmail.com>
2025-06-23 17:29:34 +0200

    Made repositories for projects, users on project and sick leave

--- AMI.EduWork.2025/AMI.EduWork.2025.Data/Repository/SickLeaveRepository.cs
+++ AMI.EduWork.2025/AMI.EduWork.2025.Data/Repository/SickLeaveRepository.cs
@@ -1,4 +1,6 @@
 ﻿using AMI.EduWork._2025.Domain.Entities;
+using AMI.EduWork._2025.Domain.Interfaces.Repository;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,25 @@ using System.Threading.Tasks;
 
 namespace AMI.EduWork._2025.Data.Repository;
 
-public class SickLeaveRepository : Repository<SickLeave>
-{
+public class SickLeaveRepository : Repository<SickLeave>, ISickLeaveRepository {
     public SickLeaveRepository(ApplicationDbContext context) : base(context) { }
+
+    public async Task<IEnumerable<SickLeave>> GetAllByDate(DateOnly startDate, DateOnly endDate) {
+        var sickLeaves = await  _dbSet.Where(sl => sl.StartDate >= startDate && sl.EndDate <= endDate).ToListAsync();
+        return sickLeaves;
+    }
+
+    public async Task<IEnumerable<SickLeave>> GetAllByDateForUser(string userId, DateOnly startDate, DateOnly endDate) {
+        var sickLeaves = await _dbSet
+            .Where(sl => sl.UserId == userId && sl.StartDate >= startDate && sl.EndDate <= endDate)
+            .ToListAsync();
+        return sickLeaves;
+    }
+
+    public async Task<IEnumerable<SickLeave>> GetByUserId(string userId) {
+         var sickLeaves = await _dbSet
+            .Where(sl => sl.UserId == userId)
+            .ToListAsync();
+        return sickLeaves;
+    }
 }
---
+++ AMI.EduWork.2025/AMI.EduWork.2025.Domain/Interfaces/Repository/IUserOnProjectRepository.cs
@@ -0,0 +1,17 @@
+﻿using AMI.EduWork._2025.Domain.Entities;
+using AMI.EduWork._2025.Domain.IRepository.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMI.EduWork._2025.Domain.Interfaces.Repository {
+    public interface IUserOnProjectRepository : IRepository<UserOnProject>{
+        public Task<IEnumerable<UserOnProject>> GetUsersByProjectId(string projectId);
+        public Task<IEnumerable<UserOnProject>> GetProjectsByUserId(string userId);
+
+        public Task<UserOnProject> GetUserOnProject(string userId, string projectId);
+
+    }
+}
