bd66d87532b7f5062589ebe2296dd2a513a169e2
AdrianBednarek1 <add.bednarek@gmail.com>
2025-06-26 19:33:17 +0200

    overrided repository

--- AMI.EduWork.2025/AMI.EduWork.2025.Data/Repository/ContractRepository.cs
+++ AMI.EduWork.2025/AMI.EduWork.2025.Data/Repository/ContractRepository.cs
@@ -1,5 +1,6 @@
 ﻿using AMI.EduWork._2025.Domain.Entities;
 using AMI.EduWork._2025.Domain.Interfaces.Repository;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,14 @@ namespace AMI.EduWork._2025.Data.Repository
     public class ContractRepository : Repository<Contract>, IContractRepository
     {
         public ContractRepository(ApplicationDbContext context) :base(context) { }
+        public override async Task<IEnumerable<Contract>> GetAll()
+        {
+            return await base._context.Contracts.Include(x => x.User).ToListAsync();
+        }
+        public override async Task<Contract> GetById(string id)
+        {
+            return await base._context.Contracts.Include(x => x.User).Where(x => x.Id == id).FirstOrDefaultAsync();
+        }
 
     }
 }
