a1ab02716118317b31581e663ce1e3621c775b34
AdrianBednarek1 <add.bednarek@gmail.com>
2025-07-02 22:32:36 +0200

    deleted 2025. project

--- AMI.EduWork.2025/AMI.EduWork.2025.Domain/Helpers/DateExtension.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AMI.EduWork._2025.Domain.Helpers;
-
-public static class DateExtension
-{
-    public static DateTime GetDateOnly(DateTime date)
-    {
-        return new DateTime(date.Year, date.Month, date.Day, 0, 0, 0);
-    }
-}
--- AMI.EduWork.2025/AMI.EduWork.2025/Enums/TimeSliceType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AMI.EduWork._2025.Enums;
-
-public enum TimeSliceType
-{
-    Work = 1,
-    Break = 2,
-    Meeting = 3
-}
